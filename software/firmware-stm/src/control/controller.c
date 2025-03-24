6fa906c9dd517069dab46cfd8152e45fd946c826
Eryk Możdżeń <ermozdzen@gmail.com>
2025-03-25 00:36:41 +0100

    add back control

--- software/firmware-stm/src/control/controller.c
+++ software/firmware-stm/src/control/controller.c
@@ -1,7 +1,7 @@
 #include <stm32h5xx_hal.h>
 
 #include "actuate/motors.h"
-//#include "actuate/servos.h"
+#include "actuate/servos.h"
 #include "com/stream.h"
 #include "utils/task.h"
 
@@ -13,7 +13,7 @@ static void manual(mpack_t *mpack) {
         last = HAL_GetTick();
 
         motors_set_velocity(manual[2], manual[5]);
-        //servos_set_position(manual[0], manual[1], manual[3], manual[4]);
+        servos_set_position(manual[0], manual[1], manual[3], manual[4]);
     }
 }
 
@@ -23,7 +23,7 @@ static void shutdown(mpack_t *mpack) {
     last = HAL_GetTick();
 
     motors_set_velocity(0, 0);
-    //servos_set_position(0, 0, 0, 0);
+    servos_set_position(0, 0, 0, 0);
 }
 
 static void watchdog() {
