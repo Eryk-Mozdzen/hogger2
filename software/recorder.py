c63021a17c4a003551df2a6e584479cc4a60f672
Eryk Możdżeń <ermozdzen@gmail.com>
2025-04-21 22:55:37 +0200

    NDJSON database recorder

---
+++ software/recorder.py
@@ -0,0 +1,24 @@
+import zmq
+import json
+import datetime
+
+context = zmq.Context()
+subscriber = context.socket(zmq.SUB)
+subscriber.connect("tcp://localhost:6000")
+subscriber.setsockopt(zmq.SUBSCRIBE, b'')
+
+filename = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S.ndjson')
+
+try:
+    with open(filename, 'w') as file:
+        while True:
+            message = subscriber.recv_json()
+            if "telemetry" in message:
+                file.write(json.dumps(message) + '\n')
+                file.flush()
+
+except KeyboardInterrupt:
+    print("\nInterrupt received, stopping...")
+finally:
+    subscriber.close()
+    context.term()
